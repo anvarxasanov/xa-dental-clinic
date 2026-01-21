800b1efd6b0e1af8e812d921fa7e172466c37c42
7lucky-axolotl228 <7error228@gmail.com>
2026-01-21 21:00:30 +0500

    Make responsive with js

---
+++ main.js
@@ -0,0 +1,24 @@
+// const btnToggler = document.querySelector('.site-header__toggler');
+
+
+// btnToggler.addEventListener('click', () => {
+    //     box.classList.toggle('site-header__hidden-box--active');
+    // })
+    
+const btnToggler = document.querySelector('.site-header__toggler');
+const header = document.querySelector('.site-header');
+const box = document.querySelector('.site-header__hidden-box');
+
+btnToggler.addEventListener('click', () => {
+    // Переключаем класс на самом родителе
+    header.classList.toggle('site-header--open');
+    box.classList.toggle('site-header__hidden-box--active');
+});
+
+// Дополнительный бонус: закрываем меню, если окно увеличили
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 1024) {
+        header.classList.remove('site-header--open');
+        box.classList.remove('site-header__hidden-box--active');
+    }
+});
